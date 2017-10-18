d3a1c7336ee7f0669288a47208ffc4f619d02edf
David Squire <dsquire@travisoft.com>
2017-10-18 16:48:34 -0700

    hacked window dragging

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfPractice
 {
@@ -24,6 +12,27 @@ namespace WpfPractice
         {
             DataContext = mainWindowViewmodel;
             InitializeComponent();
+        }
+
+        private void Titlebar_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left) return;
+            if (e.ClickCount == 2)
+            {
+                AdjustWindowSize();
+            }
+            else
+            {
+                Application.Current.MainWindow.DragMove();
+            }
+        }
+
+        /// <summary>
+        /// Adjusts the WindowSize to correct parameters when Maximize button is clicked
+        /// </summary>
+        private void AdjustWindowSize()
+        {
+            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
         }
     }
 }
