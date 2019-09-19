633c6c899f37c90a24413a4232fa2b6fd13aa7fc
David Squire <squiredd@gmail.com>
2019-09-19 10:33:28 -0700

    added netcore console app

---
+++ NetCoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shoutr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
