8c81ff30075eb2a9b7e30dfa3067f5c5fde789b1
David Squire <squiredd@gmail.com>
2019-10-02 14:46:11 -0700

    additional payload cache test

--- Library.Tests/Message/NaivePayloadCacheTests.cs
+++ Library.Tests/Message/NaivePayloadCacheTests.cs
@@ -47,52 +47,83 @@ namespace Library.Tests.Message
         [Fact]
         public async Task Handle_FileReadyWhenPayloadCached_EmitsPayload()
         {
-            try
-            {
-                // Arrange
-                var naivePayloadCache = this.CreateNaivePayloadCache();
-                Guid broadcastId = Guid.Parse("58f5022e-0024-4105-a0b8-9c77b0ead541");
-                var observable = new BehaviorSubject<IFileReadyMessage>(new FileReadyMessage(broadcastId, "file name", 0));
+            // Arrange
+            var naivePayloadCache = this.CreateNaivePayloadCache();
+            Guid broadcastId = Guid.Parse("58f5022e-0024-4105-a0b8-9c77b0ead541");
+            var observable = new BehaviorSubject<IFileReadyMessage>(new FileReadyMessage(broadcastId, "file name", 0));
 
-                var payloadMessages = new ConcurrentQueue<IPayloadMessage>();
-                var expected = new byte[]{16,16,99};
-                payloadMessages.Enqueue(new PayloadMessage(broadcastId, 0, expected, 0));
-                
-                _fileReadyCacheEntry
-                    .SetupSet(pce=>pce.SlidingExpiration = It.IsAny<TimeSpan?>())
-                    .Verifiable();
+            var payloadMessages = new ConcurrentQueue<IPayloadMessage>();
+            var expected = new byte[] {16, 16, 99};
+            payloadMessages.Enqueue(new PayloadMessage(broadcastId, 0, expected, 0));
 
-                mockMemoryCache
-                    .SetupGetOrCreate<NaivePayloadCache.FileReadyCacheKey, FileReadyMessage>(_fileReadyCacheEntry);
+            _fileReadyCacheEntry
+                .SetupSet(pce => pce.SlidingExpiration = It.IsAny<TimeSpan?>())
+                .Verifiable();
 
-                var existingObject = (object) payloadMessages;
-                mockMemoryCache
-                    .Setup(mc => mc.TryGetValue((object)It.IsAny<NaivePayloadCache.PayloadCacheKey>(), out existingObject)) 
-                    .Returns(true);
+            mockMemoryCache
+                .SetupGetOrCreate<NaivePayloadCache.FileReadyCacheKey, FileReadyMessage>(_fileReadyCacheEntry);
 
-                mockMessageCacheConfig
-                    .SetupGet(mcc => mcc.BroadcastCacheExpiration)
-                    .Returns(TimeSpan.MaxValue);
-            
-                var actual = naivePayloadCache
-                    .CachedObservable
-                    .FirstOrDefaultAsync()
-                    .ToTask();
+            var existingObject = (object) payloadMessages;
+            mockMemoryCache
+                .Setup(mc => mc.TryGetValue((object) It.IsAny<NaivePayloadCache.PayloadCacheKey>(), out existingObject))
+                .Returns(true);
 
-                // Act
-                naivePayloadCache.Handle(
-                    observable);
+            mockMessageCacheConfig
+                .SetupGet(mcc => mcc.BroadcastCacheExpiration)
+                .Returns(TimeSpan.MaxValue);
 
-                // Assert
-                Assert.Equal(expected, (await actual).Payload);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            var actual = naivePayloadCache
+                .CachedObservable
+                .FirstOrDefaultAsync()
+                .ToTask();
+
+            // Act
+            naivePayloadCache.Handle(
+                observable);
+
+            // Assert
+            Assert.Equal(expected, (await actual).Payload);
         }
 
-        
+        [Fact]
+        public async Task Handle_FileReadyWhenPayloadCached_EmitsFilename()
+        {
+            // Arrange
+            var naivePayloadCache = this.CreateNaivePayloadCache();
+            Guid broadcastId = Guid.Parse("58f5022e-0024-4105-a0b8-9c77b0ead541");
+            var expected = "file name";
+            var observable = new BehaviorSubject<IFileReadyMessage>(new FileReadyMessage(broadcastId, expected, 0));
+
+            var payloadMessages = new ConcurrentQueue<IPayloadMessage>();
+            payloadMessages.Enqueue(new PayloadMessage(broadcastId, 0, new byte[]{16,16,99}, 0));
+            
+            _fileReadyCacheEntry
+                .SetupSet(pce=>pce.SlidingExpiration = It.IsAny<TimeSpan?>())
+                .Verifiable();
+
+            mockMemoryCache
+                .SetupGetOrCreate<NaivePayloadCache.FileReadyCacheKey, FileReadyMessage>(_fileReadyCacheEntry);
+
+            var existingObject = (object) payloadMessages;
+            mockMemoryCache
+                .Setup(mc => mc.TryGetValue((object)It.IsAny<NaivePayloadCache.PayloadCacheKey>(), out existingObject)) 
+                .Returns(true);
+
+            mockMessageCacheConfig
+                .SetupGet(mcc => mcc.BroadcastCacheExpiration)
+                .Returns(TimeSpan.MaxValue);
+
+            var actual = naivePayloadCache
+                .CachedObservable
+                .FirstOrDefaultAsync()
+                .ToTask();
+
+            // Act
+            naivePayloadCache.Handle(
+                observable);
+
+            // Assert
+            Assert.Equal(expected, (await actual).FileName);
+        }
     }
 }
