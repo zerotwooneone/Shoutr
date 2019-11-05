8648d983a5dc90383cc0c66a958b57556351d4e3
David Squire <squiredd@gmail.com>
2019-11-05 15:42:57 -0800

    fixing bad code that got checked in

--- NetCoreConsoleApp/Program.cs
+++ NetCoreConsoleApp/Program.cs
@@ -37,53 +37,14 @@ namespace Shoutr
 
         private async Task Run()
         {
-            var fdr = new Library.File.NaiveFileDataRepository();
-            //var r = await fdr.SetPage("test123", 0, new byte[]{1,2,3,4,5,6,7,8,9,0});
-            //if (!r.Success)
-            //{
-            //    Console.WriteLine(r.Exception);
-            //}
+            Console.WriteLine($"Listening on port:{_configuration["listen"]}...");
 
-            const string fileName = "glowMaggot.png";
+            var listener = _listenerFactory.CreateBroadcastListener(int.Parse(_configuration["listen"]));
+            var messages = await listener
+                .MessagesObservable
+                .FirstOrDefaultAsync();
 
-            var byteCount = fdr.GetByteCount(fileName);
-
-            var tasks = new List<Task<Library.File.FileWriteResult>>();
-            for (int taskIndex = 0; taskIndex<20000; taskIndex++)
-            {
-                var pageSize = (uint)RandomGen3.Next(1, 400);
-                    var maxPageCount = (int)(byteCount / pageSize);
-                    int pageIndex = RandomGen3.Next(0, maxPageCount);
-                var t = fdr.GetPage(fileName, (int)pageSize, pageIndex)
-                .ContinueWith(t1=>{
-                    var bytes = t1.Result.Bytes;
-                    var startIndex = pageIndex * pageSize;                    
-                    return fdr.SetPage("glowMaggot-Copy.png", pageSize * pageIndex, bytes);
-                }).Unwrap();
-                                
-                tasks.Add(t);
-            }
-
-            await Task.WhenAll(tasks);
-            foreach (var task in tasks)
-            {
-                var result = await task;
-                if (!result.Success)
-                {
-                    Console.WriteLine(result.Exception);
-                }
-            }
-            
-            //Console.WriteLine($"Listening on port:{_configuration["listen"]}...");
-
-            //var listener = _listenerFactory.CreateBroadcastListener(int.Parse(_configuration["listen"]));
-            //var messages = await listener
-            //    .MessagesObservable
-            //    .FirstOrDefaultAsync();
-            
-            //Console.WriteLine($"Received {JsonConvert.SerializeObject(messages)}");
-
-            
+            Console.WriteLine($"Received {JsonConvert.SerializeObject(messages)}");            
         }
         
         private readonly IListenerFactory _listenerFactory;
