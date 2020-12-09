9884202d467d9866ca9e053c794dc55d7e37d3d8
David Squire <squiredd@gmail.com>
2020-12-08 23:01:08 -0800

    added listen method

--- Shoutr.Console/Program.cs
+++ Shoutr.Console/Program.cs
@@ -8,6 +8,7 @@ using System.Net.Sockets;
 using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Reactive.Threading.Tasks;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,43 +29,47 @@ namespace Shoutr.Console
                 .Build();
 
             var tokenSource = new CancellationTokenSource();
+
             void OnCancelKey(object sender, ConsoleCancelEventArgs e)
             {
                 tokenSource.Cancel();
             }
+
             System.Console.CancelKeyPress += OnCancelKey;
             var program = new Program();
 
             const string fileName = "test.7z";
             const int port = 3036;
 
-            if (config["listen"] != null)
+            try
             {
-                System.Console.WriteLine("Going to listen");
-            } else if (config["broadcast"] != null)
+                if (config["listen"] != null)
+                {
+                    System.Console.WriteLine("Going to listen");
+                    program.Listen(port, tokenSource.Token, f => System.Console.WriteLine($"file complete: {f}"))
+                        .Wait(tokenSource.Token);
+                }
+                else if (config["broadcast"] != null)
+                {
+                    System.Console.WriteLine("Going to broadcast");
+                    program.Broadcast(fileName, port, tokenSource.Token)
+                        .Wait(tokenSource.Token);
+                }
+            }
+            catch (Exception e)
             {
-                System.Console.WriteLine("Going to broadcast");
-                program.Broadcast(fileName, port, tokenSource.Token)
-                    .Wait(tokenSource.Token);
+                System.Console.WriteLine($"fatal error: {e}");
             }
         }
 
-        private Program(){ }
+        private Program()
+        {
+        }
 
         private async Task Broadcast(string fileName, int port, CancellationToken programToken)
         {
             var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(programToken);
             var token = cancellationTokenSource.Token;
-
-            //ProtoMessage message;
-            //using (var memoryStream = new MemoryStream(buff))
-            //{
-            //    var protoMessage = Serializer.Deserialize<ProtoMessage>(memoryStream);
-            //    message = protoMessage;
-            //}
-
-            //System.Console.WriteLine($"Buff bytes {buff.Length}");
-            //System.Console.WriteLine($"id:{new Guid(message.BroadcastId)} {JsonConvert.SerializeObject(message)}");
 
             var file = new FileInfo(fileName);
             var stream = file.OpenRead();
@@ -72,10 +77,11 @@ namespace Shoutr.Console
             const int byteToMegabyteFactor = 1000000;
             var pageSize = 8 * byteToMegabyteFactor;
 
-            var taskPoolScheduler = System.Reactive.Concurrency.Scheduler.Default; //new TaskPoolScheduler(new TaskFactory(token));
+            var taskPoolScheduler =
+                System.Reactive.Concurrency.Scheduler.Default; //new TaskPoolScheduler(new TaskFactory(token));
             var pageObservable = Observable.While(
                 // while there is data to be read
-                () => stream.CanRead && stream.Position < stream.Length, 
+                () => stream.CanRead && stream.Position < stream.Length,
                 // iteratively invoke the observable factory, which will
                 // "recreate" it such that it will start from the current
                 // stream position - hence "0" for offset
@@ -85,7 +91,7 @@ namespace Shoutr.Console
                         var readBytes = await stream.ReadAsync(buffer, 0, pageSize, token).ConfigureAwait(false);
                         return new
                         {
-                            readBytes, 
+                            readBytes,
                             startIndex = stream.Position - readBytes,
                             buffer
                         };
@@ -120,10 +126,10 @@ namespace Shoutr.Console
                     var firstPacketIndex = page.startPayloadIndex / packetSize;
                     var hasFirstFragmented = (page.startPayloadIndex % packetSize) != 0;
                     var packetList = new List<PayloadWrapper>();
-                    
+
                     var firstPartialPacketIndex = hasFirstFragmented
                         ? firstPacketIndex
-                        : (long?)null;
+                        : (long?) null;
                     var secondPacketPayloadIndex = (firstPartialPacketIndex + 1) * packetSize;
                     var firstPartialLength = (secondPacketPayloadIndex - page.startPayloadIndex) ?? 0;
 
@@ -132,7 +138,8 @@ namespace Shoutr.Console
                         var partialBuffer = new byte[firstPartialLength];
                         Array.Copy(page.array, partialBuffer, firstPartialLength);
                         var firstPartialPacket = partialBuffer;
-                        var firstPayload = new PayloadWrapper(){PayloadIndex = firstPartialPacketIndex.Value, bytes = firstPartialPacket};
+                        var firstPayload = new PayloadWrapper()
+                            {PayloadIndex = firstPartialPacketIndex.Value, bytes = firstPartialPacket};
                         CachePartialPacket(partialPacketCache, firstPayload, packetList);
                     }
 
@@ -156,7 +163,7 @@ namespace Shoutr.Console
                         var packetBuffer = new byte[packetSize];
                         var startPageIndex = ((packetIndex - firstFullPacketIndex) * packetSize) + firstPartialLength;
                         Array.Copy(page.array, startPageIndex, packetBuffer, 0, packetSize);
-                        var payload = new PayloadWrapper(){PayloadIndex = packetIndex, bytes = packetBuffer};
+                        var payload = new PayloadWrapper() {PayloadIndex = packetIndex, bytes = packetBuffer};
                         packetList.Add(payload);
                     }
 
@@ -166,13 +173,15 @@ namespace Shoutr.Console
                         var lastPartialPageIndex = page.array.Length - lastPartialLength;
                         var lastPartialPacketIndex = lastPacketIndex;
                         Array.Copy(page.array, lastPartialPageIndex, partialBuffer, 0, lastPartialLength);
-                        var lastPayload = new PayloadWrapper(){PayloadIndex = lastPartialPacketIndex, bytes = partialBuffer};
+                        var lastPayload = new PayloadWrapper()
+                            {PayloadIndex = lastPartialPacketIndex, bytes = partialBuffer};
                         CachePartialPacket(partialPacketCache, lastPayload, packetList);
                     }
 
                     return packetList.AsEnumerable();
                 })
-                .Concat(partialPacketCache.Select(kvp => new PayloadWrapper(){PayloadIndex = kvp.Key, bytes = kvp.Value}).ToObservable());
+                .Concat(partialPacketCache
+                    .Select(kvp => new PayloadWrapper() {PayloadIndex = kvp.Key, bytes = kvp.Value}).ToObservable());
             //try
             //{
             //    await payloadObservable
@@ -209,22 +218,25 @@ namespace Shoutr.Console
                     using (var memoryStream = new MemoryStream())
                     {
                         Serializer.Serialize(memoryStream,
-                            new ProtoMessage(broadcastId, payloadWrapper.PayloadIndex, payloadWrapper.bytes, null, null, null));
+                            new ProtoMessage(broadcastId, payloadWrapper.PayloadIndex, payloadWrapper.bytes, null, null,
+                                null));
                         serializedPayload = memoryStream.ToArray();
                     }
 
                     return serializedPayload;
                 });
 
-            
+
             byte[] serializedHeader;
-            var packetCount = (long)Math.Ceiling((double) file.Length / packetSize);
+            var packetCount = (long) Math.Ceiling((double) file.Length / packetSize);
             var rebroadcastTime = TimeSpan.FromSeconds(1);
             using (var memoryStream = new MemoryStream())
             {
-                Serializer.Serialize(memoryStream, new ProtoMessage(broadcastId, null, null, packetSize, fileName, packetCount));
+                Serializer.Serialize(memoryStream,
+                    new ProtoMessage(broadcastId, null, null, packetSize, fileName, packetCount));
                 serializedHeader = memoryStream.ToArray();
             }
+
             var headerObservable = Observable.Return(serializedHeader)
                 .Concat(
                     Observable.Interval(rebroadcastTime, Scheduler.Default)
@@ -233,12 +245,12 @@ namespace Shoutr.Console
                 )
                 .Concat(Observable.Return(serializedHeader));
 
-            var packetObservable = /*headerObservable
-                .Merge(*/serializedPayloadObservable; //);
+            var packetObservable = headerObservable
+                .Merge(serializedPayloadObservable);
 
             UdpClient sender = new UdpClient(port);
             IPEndPoint destination = new IPEndPoint(IPAddress.Broadcast, port);
-            
+
             await packetObservable
                 .ObserveOn(taskPoolScheduler)
                 .SelectMany((array, index) =>
@@ -255,7 +267,8 @@ namespace Shoutr.Console
 
         }
 
-        private static void CachePartialPacket(ConcurrentDictionary<long, byte[]> partialPacketCache, PayloadWrapper payload,
+        private static void CachePartialPacket(ConcurrentDictionary<long, byte[]> partialPacketCache,
+            PayloadWrapper payload,
             List<PayloadWrapper> packetList)
         {
             var shouldDelete = false;
@@ -275,6 +288,140 @@ namespace Shoutr.Console
                 partialPacketCache.TryRemove(payload.PayloadIndex, out _);
             }
         }
+
+        private async Task Listen(int port,
+            CancellationToken programToken = default,
+            Action<string> fileCompleteCallback = null)
+        {
+            var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(programToken);
+            var token = cancellationTokenSource.Token;
+
+            var packetBufferObservable = new Subject<byte[]>();
+            var observableScheduler =
+                System.Reactive.Concurrency.Scheduler.Default; //new TaskPoolScheduler(new TaskFactory(token));
+            var protoMessageObservable = packetBufferObservable
+                .ObserveOn(observableScheduler)
+                .Select(bytes =>
+                {
+                    ProtoMessage message;
+                    using (var memoryStream = new MemoryStream(bytes))
+                    {
+                        var protoMessage = Serializer.Deserialize<ProtoMessage>(memoryStream);
+                        message = protoMessage;
+                    }
+
+                    System.Console.WriteLine(
+                        $"id:{new Guid(message.BroadcastId)} {JsonConvert.SerializeObject(message)}");
+                    return message;
+                });
+
+            var headerCache = new ConcurrentDictionary<Guid, ProtoMessage>();
+            var payloadCache = new ConcurrentDictionary<Guid, List<ProtoMessage>>();
+
+            var fileWriteRequestSubject = new Subject<FileWriteWrapper>();
+
+            var headerObservable = protoMessageObservable
+                .Where(message => !string.IsNullOrWhiteSpace(message.FileName) && message.PayloadCount.HasValue);
+
+            var payloadObservable = protoMessageObservable
+                .Where(message => message.PayloadIndex.HasValue && message.Payload != null);
+
+            headerObservable
+                .ObserveOn(observableScheduler)
+                .Subscribe(header => headerCache.AddOrUpdate(header.GetBroadcastId(),
+                    bcid =>
+                    {
+                        if (payloadCache.TryRemove(bcid, out var payloads))
+                        {
+                            foreach (var payload in payloads)
+                            {
+                                fileWriteRequestSubject.OnNext(new FileWriteWrapper() {Header = header, Payload = payload});
+                            }
+                        }
+                        return header;
+                    },
+                    (bcid, m) => m));
+
+
+            payloadObservable
+                .ObserveOn(observableScheduler)
+                .Subscribe(payload =>
+                {
+                    if (headerCache.TryGetValue(payload.GetBroadcastId(), out var header))
+                    {
+                        fileWriteRequestSubject.OnNext(new FileWriteWrapper() {Header = header, Payload = payload});
+                    }
+                    else
+                    {
+                        payloadCache.AddOrUpdate(payload.GetBroadcastId(),
+                            bcid =>
+                            {
+                                var list = new List<ProtoMessage>();
+                                list.Add(payload);
+                                return list;
+                            },
+                            (bcid, list) =>
+                            {
+                                list.Add(payload);
+                                return list;
+                            });
+                    }
+                });
+
+            var writeCompleteObservable = fileWriteRequestSubject
+                .ObserveOn(observableScheduler)
+                .SelectMany(writeRequest =>
+                {
+                    return Observable.FromAsync(async () =>
+                    {
+                        var file = new FileInfo($"{System.DateTime.Now:HHmmssffff}.{writeRequest.Header.FileName}");
+                        var stream = file.OpenWrite();
+                        var writeIndex = writeRequest.Payload.PayloadIndex.Value * writeRequest.Header.PayloadMaxSize.Value;
+                        stream.Seek(writeIndex, SeekOrigin.Begin);
+                        await stream.WriteAsync(writeRequest.Payload.Payload, token).ConfigureAwait(false);
+                        return writeRequest.Header;
+                    });
+                });
+
+            var fileWriteObservable = writeCompleteObservable
+                .ObserveOn(observableScheduler)
+                .GroupBy(w => w.GetBroadcastId());
+
+            var fileTimeout = TimeSpan.FromSeconds(10);
+            Func<IGroupedObservable<Guid, ProtoMessage>, IObservable<ProtoMessage>> createAmb = null;
+            IObservable<ProtoMessage> CreateAmb(IGroupedObservable<Guid, ProtoMessage> g)
+            {
+                var nextAmb = g.FirstOrDefaultAsync().Select(x=>createAmb(g)).Switch();
+                var timeoutObservable = (IObservable<ProtoMessage>)Observable.Return(g).FirstOrDefaultAsync().Delay(fileTimeout);
+                return Observable.Amb( nextAmb, timeoutObservable);
+            }
+            createAmb = CreateAmb;
+
+            var fileStoppedObservable = fileWriteObservable
+                .SelectMany(g => CreateAmb(g))
+                .Select(p =>
+                {
+                    fileCompleteCallback?.Invoke(p.FileName);
+                    return p.FileName;
+                });
+
+            var fileStoppedSub = fileStoppedObservable.Subscribe();
+            token.Register(() => fileStoppedSub.Dispose());
+
+            UdpClient receiver = new UdpClient(3036);
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            while (!token.IsCancellationRequested)
+            {
+                var received = await receiver.ReceiveAsync().ConfigureAwait(false);
+                System.Console.WriteLine($"Buff bytes {received.Buffer.Length}");
+                packetBufferObservable.OnNext(received.Buffer);
+            }
+        }
+    }
+
+    internal record FileWriteWrapper {
+        internal ProtoMessage Header { get; init; }
+        internal ProtoMessage Payload { get; init; }
     }
 
     internal record PayloadWrapper
@@ -315,6 +462,11 @@ namespace Shoutr.Console
             PayloadMaxSize = payloadMaxSize;
             FileName = fileName;
             PayloadCount = payloadCount;
+        }
+
+        internal Guid GetBroadcastId()
+        {
+            return new Guid(BroadcastId);
         }
     }
 }
