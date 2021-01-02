f921fb2973036cc827046a07b9d421b3064beeea
David Squire <squiredd@gmail.com>
2021-01-02 01:15:14 -0800

    add header test

--- Shoutr.Tests/BroadcasterTests.cs
+++ Shoutr.Tests/BroadcasterTests.cs
@@ -17,20 +17,53 @@ namespace Shoutr.Tests
         }
 
         [Test]
-        public async Task BroadcastTest()
-        {
+        public async Task GetHeaderObservable_ReturnsFirstAndLast_WhenOnePayloads()
+        {   
             var bc = new Broadcaster();
             var serializedHeader = new byte[] { 1, 2, 3, 4};
             const int rebroadcastSeconds = 1;
-            var payloadObservable = Observable.Empty<byte[]>();
+            var payloadObservable = Observable.Return(new byte[] { 9, 9, 9});
             var observable = bc.GetHeaderObservable(serializedHeader,
                 TimeSpan.FromSeconds(rebroadcastSeconds),
                 payloadObservable,
                 _scheduler);
 
             var headers = await observable.ToArray();
+            Assert.AreEqual(2, headers.Length);
             CollectionAssert.AreEqual(serializedHeader, headers[0]);
             CollectionAssert.AreEqual(serializedHeader, headers[1]);
+        }
+        
+        [Test]
+        public async Task GetHeaderObservable_ReturnsExtra_WhenOnePayloadDelayed()
+        {   
+            var bc = new Broadcaster();
+            var serializedHeader = new byte[] { 1, 2, 3, 4};
+            var rebroadcastSeconds = TimeSpan.FromSeconds(1);
+            var payloadObservable = Observable
+                .Return(new byte[] { 9, 9, 9})
+                .Delay(rebroadcastSeconds.Add(TimeSpan.FromTicks(1)), _scheduler)
+                .Concat(Observable.Never<byte[]>());
+
+            byte[] mostRecent = null;
+            int count = 0;
+            var sub = bc.GetHeaderObservable(serializedHeader,
+                    rebroadcastSeconds,
+                payloadObservable,
+                _scheduler)
+                .Subscribe(b =>
+                {
+                    mostRecent = b;
+                    count++;
+                });
+            
+            CollectionAssert.AreEqual(serializedHeader, mostRecent);
+            Assert.AreEqual(1, count);
+            
+            _scheduler.AdvanceBy(rebroadcastSeconds);
+
+            CollectionAssert.AreEqual(serializedHeader, mostRecent);
+            Assert.AreEqual(2, count);
         }
     }
 }
---
+++ Shoutr.Tests/TestSchedulerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Reactive.Testing;
+
+namespace Shoutr.Tests
+{
+    public static class TestSchedulerExtensions
+    {
+        public static void AdvanceBy(this TestScheduler testScheduler, TimeSpan timeSpan)
+        {
+            testScheduler.AdvanceBy(timeSpan.Ticks);
+        }
+    }
+}
