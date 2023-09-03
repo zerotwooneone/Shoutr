30adc90884db80ca4d4233a774186f28796d7c39
zero <squiredd@gmail.com>
2023-09-03 16:27:07 -0700

    make broadcasts cards

--- source/ShoutrApp/src/app/material/material.module.ts
+++ source/ShoutrApp/src/app/material/material.module.ts
@@ -4,6 +4,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 //note: this is just a convenient place to put all the material module references
 //  to avoid cluttering the app module
@@ -17,6 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatProgressBarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
   ]
 })
 export class MaterialModule { }
